ab08ce55bd84c744fcbe2b68fa467379620468a6
Micah Conn <mcoxconn@gmail.com>
2021-10-25 15:12:59 -0500

    fixed home/need to add a static image or text to home route

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <div className="container">
       <CategoryMenu />
-      <MiniatureList />
-      <Cart />
+      {/* <MiniatureList />
+      <Cart /> */}
     </div>
   );
 };
