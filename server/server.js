7144de1bf7c8bf0b602b6825b5dc1028e5dd4fe9
Micah Conn <mcoxconn@gmail.com>
2021-10-28 19:39:47 -0500

    production was changed ln25 server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-if (process.env.NODE_ENV === 'miniatureion') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
